2160c264254d753fdc482bf9131b45edde4d604c
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-04-02 17:07:32 +0200

    lista film + votazione in pagina

--- index.php
+++ index.php
@@ -20,20 +20,86 @@ class Movie
    public $year;
    public $genere;
    public $regista;
+   public $votation;
    
 
 //costruttore
 
-function __construct($title,$year,$genere)
+function __construct($title,$year,$genere,$votation)
 
 {
    $this->title = $title;
    $this->year = $year;
    $this->genere = $genere;
+   $this->votation = $votation;
+   
 }
 
+public function isRecommended() 
+{
+   if ($this->votation > 7.5) {
+      return "Consigliato";
+  } else {
+      return "Non è consigliato";
+  }
+}
+ 
+
 }
 
-$pulpFiction = new Movie('Pulp Fiction', 1994, 'Thriller-pulp ');
+// Creazione oggetti Movie
+$movie1 = new Movie("Interstellar", 2014, "Science Fiction", 8.6);
+$movie2 = new Movie("Inception", 2010, "Science Fiction", 8.8);
+$movie3 = new Movie('Pulp Fiction', 1994, 'Thriller-pulp',9);
+$movie4 = new Movie('Natale in India', 2003, 'commedia',3.6);
 
-var_dump($pulpFiction);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>php-Movie</title>
+</head>
+<body>
+   <h1>Movies</h1>
+
+   <?php
+   // Stampo a schermo i valori delle relative proprietà e se il film è consigliato o meno
+echo "<h2>Film 1:</h2>";
+echo "Titolo: " . $movie1->title . "<br>";
+echo "Anno: " . $movie1->year . "<br>";
+echo "Genere: " . $movie1->genere . "<br>";
+echo "Votazione: " . $movie1->votation . "<br>";
+echo "Consigliato? " . $movie1->isRecommended() . "<br>";
+
+echo "<h2>Film 2:</h2>";
+echo "Titolo: " . $movie2->title . "<br>";
+echo "Anno: " . $movie2->year . "<br>";
+echo "Genere: " . $movie2->genere . "<br>";
+echo "Votazione: " . $movie2->votation . "<br>";
+echo "Consigliato? " . $movie2->isRecommended() . "<br>";
+
+echo "<h2>Film 3:</h2>";
+echo "Titolo: " . $movie3->title . "<br>";
+echo "Anno: " . $movie3->year . "<br>";
+echo "Genere: " . $movie3->genere . "<br>";
+echo "Votazione: " . $movie3->votation . "<br>";
+echo "Consigliato? " . $movie3->isRecommended() . "<br>";
+
+echo "<h2>Film 4:</h2>";
+echo "Titolo: " . $movie4->title . "<br>";
+echo "Anno: " . $movie4->year . "<br>";
+echo "Genere: " . $movie4->genere . "<br>";
+
+echo "Votazione: " . $movie4->votation . "<br>";
+echo "Consigliato? " . $movie4->isRecommended() . "<br>";
+
+?>
+
+</body>
+</html>
